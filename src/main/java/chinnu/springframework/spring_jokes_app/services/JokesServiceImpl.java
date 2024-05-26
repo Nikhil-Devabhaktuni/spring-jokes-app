c40002b2f318e3a6ec5594f71812d7595dd00d5b
Nikhil-Devabhaktuni <nikhildevabhaktuni@outlook.com>
2024-05-26 12:23:56 +0100

    Controller
    this closes issue #3.

---
+++ src/main/java/chinnu/springframework/spring_jokes_app/services/JokesServiceImpl.java
@@ -0,0 +1,22 @@
+package chinnu.springframework.spring_jokes_app.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokeService{
+
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    @Autowired
+    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes;
+    }
+}
